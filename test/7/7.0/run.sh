5269a623cab31cbdbc5b6b22fdddeb891136c2ce
Chingis Sandanov <chingis@wodby.com>
2017-10-19 18:07:01 +0700

    Revamp tests

--- test/7/7.0/run.sh
+++ test/7/7.0/run.sh
@@ -12,6 +12,7 @@ docker-compose up -d
 docker-compose exec mariadb make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
 docker-compose exec solr make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=3
 docker-compose exec solr make core=core1 -f /usr/local/bin/actions.mk
+docker-compose exec --user=0 php apk add --update jq
 docker-compose exec --user=0 php chown -R www-data:www-data /var/www/html
 docker-compose exec php ./test.sh
 docker-compose down
