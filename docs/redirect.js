03d86130fe50fdaa11f362548554a346ec1588ac
Chingis Sandanov <chingis@wodby.com>
2017-12-26 17:53:34 +0700

    Add redirect from old docs

---
+++ docs/redirect.js
@@ -0,0 +1,4 @@
+if (String(window.location).indexOf("readthedocs") !== -1) {
+    window.alert('The documentation has moved. I will redirect you to the new location.');
+    window.location.replace('http://wodby.com/stacks/drupal/docs/local');
+}
