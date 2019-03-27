f52be1e7695960326f5bca3c8656db6757453108
Irmantas Puodžiukas <irmantas.puodziukas@gmail.com>
2019-03-27 13:47:34 +0200

    Clarify optional parameters for  command

--- docker.mk
+++ docker.mk
@@ -33,7 +33,7 @@ prune:
 ps:
 	@docker ps --filter name='$(PROJECT_NAME)*'
 
-## shell	:	Remote in to runninng `php` container.
+## shell	:	Access `php` container via shell.
 shell:
 	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='$(PROJECT_NAME)_php' --format "{{ .ID }}") sh
 
@@ -43,6 +43,9 @@ drush:
 	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") drush -r $(DRUPAL_ROOT) $(filter-out $@,$(MAKECMDGOALS))
 
 ## logs	:	View containers logs.
+##		You can optinally pass an argument with the service name to limit logs
+##		logs php	: View `php` container logs.
+##		logs nginx php	: View `nginx` and `php` containers logs.
 logs:
 	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
