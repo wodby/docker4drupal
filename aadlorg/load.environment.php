fd22d32dae426e58c5b0862651eef189020c4133
Laurie Kirchmeier <kirchmeierl@aadl.org>
2021-03-26 23:20:29 -0400

    Working single composer.json test using "composer install" command

---
+++ aadlorg/load.environment.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is included very early. See autoload.files in composer.json and
+ * https://getcomposer.org/doc/04-schema.md#files
+ */
+
+use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
+
+/**
+ * Load any .env file. See /.env.example.
+ */
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
