e0156c405d3532167fa155255b0e6775521c813a
Jose Ramirez <webmaster@reapbooster.com>
2021-04-25 00:33:13 +0000

    Create Custom Bootstrap Barrio Theme :rocket:

---
+++ codebase/web/themes/custom/custom_bootstrap_barrio/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.custom_bootstrap_barrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
