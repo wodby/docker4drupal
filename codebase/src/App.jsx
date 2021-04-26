a5a95bb0107904ba32183ebcd1ad85fcf8c41926
Jose Ramirez <webmaster@reapbooster.com>
2021-04-26 06:45:37 +0000

    Add a test ReactJS component :wrench:

---
+++ codebase/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {likesCount : 0};
+    this.onLike = this.onLike.bind(this);
+  }
+
+  onLike () {
+    let newLikesCount = this.state.likesCount + 1;
+    this.setState({likesCount: newLikesCount});
+  }
+
+  render() {
+    return (
+      <div className="my-5">
+        Likes : <span>{this.state.likesCount}</span>
+        <div><button onClick={this.onLike}>Like Me</button></div>
+      </div>
+    );
+  }
+
+}
+
+export default App;
