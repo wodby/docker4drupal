b3f9fbb287da854223ca00b1b9acadbe3ca90f51
Chingis Sandanov <chingis@wodby.com>
2022-01-15 15:42:51 +0800

    Update drush version in tests

--- tests/9/tests.sh
+++ tests/9/tests.sh
@@ -41,7 +41,7 @@ cd ./web
 drush si -y --db-url="${DB_URL}"
 
 # Test Drupal status and requirements
-check_status "drush-version" "10.*"
+check_status "drush-version" "11.*"
 check_status "root" "${APP_ROOT}/${DOCROOT_SUBDIR}"
 check_status "site" "sites/default"
 check_status "files" "sites/default/files"
