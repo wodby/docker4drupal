f460588c091ca51684f3082c7dbaa0a1a3d9846e
Chingis Sandanov <chingis@wodby.com>
2024-04-04 10:34:23 +0300

    Update docker-compose to new format

--- tests/10/run.sh
+++ tests/10/run.sh
@@ -6,11 +6,11 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-docker-compose up -d
-docker-compose exec -T mariadb make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
-docker-compose exec -T solr make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=3
-docker-compose exec -T solr make init -f /usr/local/bin/actions.mk
-docker-compose exec -T --user=0 php apk add --update jq
-docker-compose exec -T --user=0 php chown -R wodby:wodby /var/www/html
-docker-compose exec -T php tests.sh
-docker-compose down
+docker compose up -d
+docker compose exec -T mariadb make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
+docker compose exec -T solr make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=3
+docker compose exec -T solr make init -f /usr/local/bin/actions.mk
+docker compose exec -T --user=0 php apk add --update jq
+docker compose exec -T --user=0 php chown -R wodby:wodby /var/www/html
+docker compose exec -T php tests.sh
+docker compose down
