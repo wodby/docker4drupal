a16a372b88fc1ad0117d33803c7f6f36b30dd1a4
Chingis Sandanov <chingis@chingis.io>
2025-09-10 16:23:14 +0200

    Return drush and solr tests

--- tests/cms/tests.sh
+++ tests/cms/tests.sh
@@ -23,14 +23,13 @@ DB_URL="${DB_DRIVER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}"
 make init -f /usr/local/bin/actions.mk
 
 composer require -n \
-  drupal/redis
-#  \
-#  drupal/purge \
-#  drupal/varnish_purge
+  drupal/redis \
+  drupal/purge \
+  drupal/varnish_purge
 
-#composer require -n \
-#  drupal/search_api \
-#  drupal/search_api_solr:~4
+composer require -n \
+  drupal/search_api \
+  drupal/search_api_solr:~4
 
 cd ./web
 
@@ -51,15 +50,14 @@ check_rq "File system" "Writable"
 check_rq "Configuration files" "Protected"
 
 drush en -y \
-  redis
-##   \
-##  purge \
-##  purge_queuer_coretags \
-##  purge_drush \
-##  varnish_purger \
-##  varnish_purge_tags
+  redis \
+  purge \
+  purge_queuer_coretags \
+  purge_drush \
+  varnish_purger \
+  varnish_purge_tags
 
-#drush en -y search_api_solr_admin
+drush en -y search_api_solr_admin
 
 chmod 755 "${PWD}/sites/default/settings.php"
 echo "include '${PWD}/sites/default/test.settings.php';" >>"${PWD}/sites/default/settings.php"
@@ -70,30 +68,30 @@ check_rq "Redis" "Connected"
 check_rq "Trusted Host Settings" "Enabled"
 
 # Import solr server
-#drush cim --source=/var/www/html/solr --partial -y
-#drush solr-upload-conf solr 1
-#drush sapi-sl | grep -q enabled
+drush cim --source=/var/www/html/solr --partial -y
+drush solr-upload-conf solr
+drush sapi-sl | grep -q enabled
 
 ## Test varnish cache and purge
-#drush ppadd varnish
-#drush cr
+drush ppadd varnish
+drush cr
 
 ## Workaround for varnish purger import https://www.drupal.org/node/2856221
-#PURGER_ID=$(drush ppls --format=json | jq -r "keys[0]")
-#
-#mkdir -p /var/www/html/varnish
-## We copy mounted file because we can't edit mounted file (resource busy error).
-#cp /var/www/html/varnish-purger.yml /var/www/html/varnish/purger.yml
-#sed -i "s/PLUGIN_ID/${PURGER_ID}/g" /var/www/html/varnish/purger.yml
-#mv /var/www/html/varnish/purger.yml "/var/www/html/varnish/varnish_purger.settings.${PURGER_ID}.yml"
-#drush -y cim --source=/var/www/html/varnish --partial
-#drush -y config-set system.performance cache.page.max_age 43200
-#
-#curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: MISS"
-#curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: HIT"
-#
-#drush cc render
-#drush pqw
-#
-#curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: MISS"
-#curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: HIT"
+PURGER_ID=$(drush ppls --format=json | jq -r "keys[0]")
+
+mkdir -p /var/www/html/varnish
+# We copy mounted file because we can't edit mounted file (resource busy error).
+cp /var/www/html/varnish-purger.yml /var/www/html/varnish/purger.yml
+sed -i "s/PLUGIN_ID/${PURGER_ID}/g" /var/www/html/varnish/purger.yml
+mv /var/www/html/varnish/purger.yml "/var/www/html/varnish/varnish_purger.settings.${PURGER_ID}.yml"
+drush -y cim --source=/var/www/html/varnish --partial
+drush -y config-set system.performance cache.page.max_age 43200
+
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: MISS"
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: HIT"
+
+drush cc render
+drush pqw
+
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: MISS"
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: HIT"
