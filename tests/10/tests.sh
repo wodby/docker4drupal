e61a633973f426870f1692a9d7c59a99bc29a4c2
Chingis Sandanov <chingis@chingis.io>
2026-02-19 00:25:54 +0800

    Drop php 8.1

--- tests/10/tests.sh
+++ tests/10/tests.sh
@@ -36,11 +36,7 @@ cd ./web
 drush si -y --db-url="${DB_URL}"
 
 # Test Drupal status and requirements
-if [[ "${PHP_VERSION}" = 8.1* ]]; then
-  check_status "drush-version" "12.*"
-else
-  check_status "drush-version" "13.*"
-fi
+check_status "drush-version" "13.*"
 
 check_status "root" "${APP_ROOT}/${DOCROOT_SUBDIR}"
 check_status "site" "sites/default"
